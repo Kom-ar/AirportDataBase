f20bddea7a4eea7e42d07bdf8543bbc116979936
Kom-ar <diman141141@gmail.com>
2023-04-27 01:10:46 +1000

    Change login, change password

--- Users.cpp
+++ Users.cpp
@@ -22,9 +22,10 @@ int Users::log_in(string log, string pass_w) {
 
 }
 
-int Users::registration(string log, string passw, string first_name, string last_name, int passport_number) {
+int Users::registration(string log, string passw, string first_name, string last_name, string passport_number) {
 	ifstream r_usr;
 	ofstream w_usr;
+	int ts = 0, b_letter = 0, sm_letter = 0;
 	r_usr.open("Users.txt", ios::in);
 	w_usr.open("Users.txt", ios::app);
 	// проверка логина на уникальность
@@ -40,10 +41,139 @@ int Users::registration(string log, string passw, string first_name, string last
 	r_usr.close();
 
 	// проверка парол€
-	// проверка фамиилии, имени, паспортных данных
+	for (int i; i < passw.size(); i++) {
+		if (passw.size() >= 6 || passw.size() <= 20) {
+			if (('0' <= passw[i]) && (passw[i] <= '9')) {
+				ts++;
+			}
+			if (('A' <= passw[i]) && (passw[i] <= 'Z')) {
+				b_letter++;
+			}
+			if (('a' <= passw[i]) && (passw[i] <= 'z')) {
+				sm_letter++;
+			}
+		}
+	}
+	if ((b_letter == 0) || (sm_letter == 0) || (ts == 0))
+		return 0;
+
+	//проверка паспортных данных
+	for (int i; i < passport_number.size(); i++) {
+		if (passport_number.size() == 10) {
+			if (('0' > passport_number[i]) && (passport_number[i] > '9'))
+				return 0;
+		}
+	}
+
+	//ѕроверка фамилии и имени
+	for (int i; i < first_name.size(); i++) {
+		if (('0' <= first_name[i]) && (first_name[i] <= '9'))
+			return 0;
+	}
+	for (int i; i < last_name.size(); i++) {
+		if (('0' <= last_name[i]) && (last_name[i] <= '9'))
+			return 0;
+	}
 
 	//запись данных в файл
 	w_usr << log << passw << first_name << last_name << passport_number << 0;
 	w_usr.close();
 	return 1;
+}
+
+string Users::get_login() {
+	return this->login;
+}
+
+string Users::get_password() {
+	return this->password;
+}
+
+void Users::set_login(string new_login) {
+	this->login = new_login;
+}
+void Users::set_password(string new_password) {
+	this->password = new_password;
+}
+
+int Users::change_login(string new_login) {
+	string old_login = get_login();
+	ifstream r_usr;
+	r_usr.open("Users.txt", ios::in);
+	string usr_log;
+	// проверка логина на уникальность
+	while (!r_usr.eof()) {
+		r_usr >> usr_log;
+		if (usr_log == new_login) {
+			r_usr.close();
+			return 0;
+		}
+	}
+	r_usr.close();
+	//»зменение логина в файле Users.txt
+	r_usr.open("Users.txt", ios::in);
+	ofstream new_usr;
+	new_usr.open("new_Users.txt", ios::out);
+	while (!r_usr.eof()) {
+		r_usr >> usr_log;
+		if (usr_log == old_login) {
+			new_usr << new_login;
+			continue;
+		}
+		new_usr << usr_log;
+	}
+	r_usr.close();
+	new_usr.close();
+
+	system("del Users.txt");
+	system("ren new_Users.txt Users.txt");
+	set_login(new_login);
+	return 1;
+
+}
+
+int Users::change_password(string new_password) {
+	string login = get_login();
+	//ѕроверка парол€
+	int ts = 0, b_letter = 0, sm_letter = 0;
+	for (int i; i < new_password.size(); i++) {
+		if (new_password.size() >= 6 || new_password.size() <= 20) {
+			if (('0' <= new_password[i]) && (new_password[i] <= '9')) {
+				ts++;
+			}
+			if (('A' <= new_password[i]) && (new_password[i] <= 'Z')) {
+				b_letter++;
+			}
+			if (('a' <= new_password[i]) && (new_password[i] <= 'z')) {
+				sm_letter++;
+			}
+		}
+	}
+	if ((b_letter == 0) || (sm_letter == 0) || (ts == 0))
+		return 0;
+
+	ifstream r_usr;
+	r_usr.open("Users.txt", ios::in);
+	string usr_log;
+	//»зменение парол€ в файле Users.txt
+	ofstream new_usr;
+	new_usr.open("new_Users.txt", ios::out);
+	while (!r_usr.eof()) {
+		r_usr >> usr_log;
+		if (usr_log == login) {
+			new_usr << login;
+			r_usr >> usr_log;
+			new_usr << new_password;
+			continue;
+		}
+		new_usr << usr_log;
+	}
+	r_usr.close();
+	new_usr.close();
+
+	system("del Users.txt");
+	system("ren new_Users.txt Users.txt");
+	set_password(new_password);
+	return 1;
+
 }
--- Users.h
+++ Users.h
@@ -11,13 +11,20 @@ private:
 	string password;
 	string first_name;
 	string last_name;
-	int passport_number;
-	int is_admin;
+	string passport_number;
+	string is_admin;
 	int balance;
 
 public:
 	int log_in(string log, string pass_w);
-	int registration(string log, string passw, string first_name, string last_name, int passport_number);
+	int registration(string log, string passw, string first_name, string last_name, string passport_number);
+	string get_login();
+	string get_password();
+	void set_login(string new_login);
+	void set_password(string new_password);
+	int change_login(string new_login);
+	int change_password(string new_password);
+
 
 };
 
